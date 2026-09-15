2b35b45c6e8d9975af8eef0d078a63a31738d231
Yan Vashchanka <yan.vashchenko@gmail.com>
2026-09-15 22:06:00 +0300

    feat: solve task 5

--- task5.ts
+++ task5.ts
@@ -1,6 +1,23 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 // Написать функцию, которая разворачивает вложенные массивы в один
 
 const array = [1, [2, 3], [4], 5, [6, 7, 8]];
+const array2 = [1, [2, 3], [4], 5, [6, 7, 8, [9, 10, 11]]];
 
-const result = [1, 2, 3, 4, 5, 6, 7, 8];
+// const result = [1, 2, 3, 4, 5, 6, 7, 8];
+
+type RecursiveType<T> = (T | RecursiveType<T>)[];
+
+function flattenArr<T>(arr: RecursiveType<T>) {
+  const flatArr: T[] = [];
+  for (const i of arr) {
+    if (Array.isArray(i)) {
+      flatArr.push(...flattenArr(i));
+    } else {
+      flatArr.push(i);
+    }
+  }
+  return flatArr;
+}
+
+console.log(flattenArr(array));
+console.log(flattenArr(array2));
