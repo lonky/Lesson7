876a1bb15640b14023ae04aee6db0df0d4da237e
Yan Vashchanka <yan.vashchenko@gmail.com>
2026-09-15 21:23:21 +0300

    feat: task 3 and 4 done

--- task4.ts
+++ task4.ts
@@ -1,6 +1,19 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 // Написать функцию, которая возвращает объект в виде ключ (элемент массива) и значение (сколько раз элемент повторяется)
 
 const array: string[] = ["orange", "apple", "banana", "apple", "orange", "orange"];
 
-const result: Record<string, number> = { orange: 3, apple: 2, banana: 1 };
+// const result: Record<string, number> = { orange: 3, apple: 2, banana: 1 };
+
+function countEl(arr: string[]) {
+  const counterObj: Record<string, number> = {};
+  for (const el of arr) {
+    if (!counterObj[el]) {
+      counterObj[el] = 1;
+    } else {
+      counterObj[el] += 1;
+    }
+  }
+  return counterObj;
+}
+
+console.log(countEl(array));
